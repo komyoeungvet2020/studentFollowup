ae2255038e6db316ed591632c89d90987dd223d3
komyoeungvet <komyoeung.vet@student.passerellesnumeriques.org>
2020-07-02 16:36:24 +0700

    my step8

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -125,6 +125,13 @@ class studentController extends Controller
         $student->save();
         return redirect('outFollowupView');
     }
+    public function studentfollowup($id)
+    {
+        $student = Student::find($id);
+        $student->activeFollowup = 1;
+        $student->save();
+        return redirect('home');
+    }
     public function outFollowupView(){
         $student = Student::all();
         return view('student.outfollowup', compact('student'));
